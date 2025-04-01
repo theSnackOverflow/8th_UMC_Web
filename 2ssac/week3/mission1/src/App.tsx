c7e315f2957e59a1f6361cefe406fb1a7ffcc66c
2sac <ivory.ma9ic@gmail.com>
2025-04-02 08:53:32 +0900

    chapter-3 mission1 commit

---
+++ 2ssac/week3/mission1/src/App.tsx
@@ -0,0 +1,14 @@
+// import React from "react";
+import MoviesPage from "./pages/movies";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex justify-center items-center">
+      <div className="max-w-7xl w-full px-4">
+        <MoviesPage />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ 2ssac/week3/mission1/src/components/MovieCard.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Movie } from "../types/movie";
+
+type MovieCardProps = {
+  movie: Movie;
+};
+
+const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
+  return (
+    <div className="relative w-full h-[300px] rounded-xl overflow-hidden shadow-lg group cursor-pointer">
+      <img
+        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+        alt={movie.title}
+        className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
+      />
+
+      {/* hover 시 보이는 overlay */}
+      <div className="absolute inset-0 bg-black/60 backdrop-blur-sm opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-white flex flex-col justify-end p-4">
+        <h2 className="text-lg font-bold">{movie.title}</h2>
+        <p className="text-sm line-clamp-3">{movie.overview}</p>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
