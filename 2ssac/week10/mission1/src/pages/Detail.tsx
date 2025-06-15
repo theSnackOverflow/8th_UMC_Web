5c9f0a94fde37389025e08adf7c546e4b0a66c99
2ssac <ivory.ma9ic@gmail.com>
2025-06-16 03:39:56 +0900

    #1 week10 프로젝트 세팅

---
+++ 2ssac/week10/mission1/src/pages/Detail.tsx
@@ -0,0 +1,4 @@
+const Detail = () => {
+  return <h2 className="text-2xl font-bold text-green-600">디테일 페이지</h2>
+}
+export default Detail
---
+++ 2ssac/week10/mission1/src/pages/Home.tsx
@@ -0,0 +1,4 @@
+const Home = () => {
+  return <h2 className="text-2xl font-bold text-blue-600">홈 페이지</h2>
+}
+export default Home
