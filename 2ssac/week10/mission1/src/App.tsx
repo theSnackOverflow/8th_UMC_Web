528cdaa612f6f7be731ee35274ebdfd650f281a5
2ssac <ivory.ma9ic@gmail.com>
2025-06-16 20:09:13 +0900

    #10 App.tsx 및 dir 구조 리팩토링

--- 2ssac/week10/mission1/src/App.tsx
+++ 2ssac/week10/mission1/src/App.tsx
@@ -1,19 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Layout from './layout/Layout'
-import Home from './pages/Home'
-import Detail from './pages/Detail'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './layout/Layout';
+import MovieList from './components/MovieList';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="detail/:id" element={<Detail />} />        
+          <Route index element={<MovieList />} />
         </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
